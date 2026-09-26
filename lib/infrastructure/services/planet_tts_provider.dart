4fea82312778973cb44d9e146e4f0eda4d29124c
Dika Wardani <39647829+dikawardani24@users.noreply.github.com>
2026-09-26 21:50:07 +0700

    feat: add planet TTS service

---
+++ lib/infrastructure/services/planet_tts_provider.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'planet_tts_service.dart';
+
+final planetTtsServiceProvider = Provider<PlanetTtsService>((ref) {
+  final service = PlanetTtsService();
+  ref.onDispose(service.stop);
+  return service;
+});
